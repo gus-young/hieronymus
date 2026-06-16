3a1deffaca76b23f6e1f8b91ce2701b264e4547d
Gus Young <76542590+gus-young@users.noreply.github.com>
2026-06-15 23:23:40 -0400

    ADDED - Added ClientMessage and ChatResponse classes as pydantic basemodels. Created /chat/ post response. Began work on lightweight chat html and supporting JS.

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+
+class ClientMessage(BaseModel):
+    message: str
+    session_id: str 
+
+class ChatResponse(BaseModel):
+    reply: str
+    session_id: str
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"status": "ok"}
+
+@app.post("/chat/")
+async def message_response(request: ClientMessage):
+    session_id = request.session_id
+    return {"reply": f"session id = {session_id}"}
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
---
+++ static/app.js
@@ -0,0 +1,5 @@
+const session_id = crypto.randomUUID();
+
+document.getElementById("chatForm").addEventListener('submit', (event) => {
+    event.preventDefault()
+})
